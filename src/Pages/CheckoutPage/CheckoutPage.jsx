4566b0106850cd8b685263ad9d19d6489386770a
Jianru D <dengjr1509@gmail.com>
2024-03-22 11:17:32 -0700

    add and clean repo

---
+++ src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+import "./CheckoutPage.scss";
+
+function CheckoutPage() {
+  return (
+    <main>
+      <h1>Checkout Page</h1>
+    </main>
+  );
+}
+
+export default CheckoutPage;
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import "./LandingPage.scss";
+
+function LandingPage() {
+  return (
+    <main>
+      <h1>Landing Page</h1>
+    </main>
+  );
+}
+
+export default LandingPage;
