afb493b50defd231bc5d507337144f1a6170b6be
Jianru D <dengjr1509@gmail.com>
2024-03-22 13:59:13 -0700

    add font style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import "./Header.scss";
 import logo from "../../assets/logos/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      {/* logo */}
-      <div className="header__logo">
-        <img src={logo} alt="logo" />
-      </div>
+      {/* logo link to home */}
+      <Link to="/">
+        <img src={logo} alt="Lush logo" className="header__logo" />
+      </Link>
     </header>
   );
 }
