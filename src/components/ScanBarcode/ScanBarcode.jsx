a64bd5c0cf6c6c6353878e00d7ce1fed98885126
DJANG2024 <derek.k.jang@gmail.com>
2024-03-22 12:41:49 -0700

    add barcode search form and btns

--- src/components/ScanBarcode/ScanBarcode.jsx
+++ src/components/ScanBarcode/ScanBarcode.jsx
@@ -4,19 +4,29 @@ import barcode from "../../assets/images/barcode.png";
 function ScanBarcode() {
   return (
     <div className="scan">
-      <h1>Scan Products</h1>
-      <img src={barcode} alt="a barcode" />
-      <form action="" className="scan__form">
-        <input type="text" placeholder="Search..." />
-        <div>
-          <input type="text" placeholder="Proudct" />
-          <input type="text" placeholder="Quantity" />
-        </div>
-        <div>
-          <button>Confirm</button>
-          <button>Cancel</button>
-        </div>
-      </form>
+      <div className="scan__container">
+        <h1 className="scan__title">SCAN PRODUCTS</h1>
+        <img src={barcode} alt="a barcode" className="scan__img" />
+        <form action="" className="scan__form">
+          <input
+            type="text"
+            placeholder="SEARCH PRODUCT..."
+            className="scan__search"
+          />
+          <div className="scan__container2">
+            <input
+              type="text"
+              placeholder="SELECTED PRODUCT"
+              className="scan__product"
+            />
+            <input type="text" placeholder="QUANTITY" className="scan__qty" />
+          </div>
+          <div className="scan__container3">
+            <button className="scan__btn-confirm">CONFIRM</button>
+            <button className="scan__btn-cancel">CANCEL</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
