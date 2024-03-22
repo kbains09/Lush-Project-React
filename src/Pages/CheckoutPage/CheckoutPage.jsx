a07b54a5bf7dd091009f1ed270676872a754def8
DJANG2024 <derek.k.jang@gmail.com>
2024-03-22 11:43:40 -0700

    add basic form to scanbarcode component

--- src/Pages/CheckoutPage/CheckoutPage.jsx
+++ src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -1,8 +1,10 @@
+import ScanBarcode from "../../components/ScanBarcode/ScanBarcode";
 import "./CheckoutPage.scss";
 
 function CheckoutPage() {
   return (
     <main>
+      <ScanBarcode />
       <h1>Checkout Page</h1>
     </main>
   );
--- src/components/ScanBarcode/ScanBarcode.jsx
+++ src/components/ScanBarcode/ScanBarcode.jsx
@@ -1,8 +1,24 @@
+import "./ScanBarcode.scss";
+import barcode from "../../assets/images/barcode.png";
+
 function ScanBarcode() {
-  return;
-  <div className="scanbarcode">
-    <h1>Scan Products</h1>
-  </div>;
+  return (
+    <div className="scan">
+      <h1>Scan Products</h1>
+      <img src={barcode} alt="a barcode" />
+      <form action="" className="scan__form">
+        <input type="text" placeholder="Search..." />
+        <div>
+          <input type="text" placeholder="Proudct" />
+          <input type="text" placeholder="Quantity" />
+        </div>
+        <div>
+          <button>Confirm</button>
+          <button>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default ScanBarcode;
