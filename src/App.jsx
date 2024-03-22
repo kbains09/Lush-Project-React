ae4f929495a766258b6905e12d30b0ce5f6ebdfe
Jianru D <dengjr1509@gmail.com>
2024-03-22 10:29:55 -0700

    add styles and routers

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+// import pages
+import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
