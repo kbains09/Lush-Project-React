4a00139882fbbc92f1cdd28f97f3e12922343ad4
DJANG2024 <derek.k.jang@gmail.com>
2024-03-22 11:23:11 -0700

    start checkout folder

---
+++ src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+import "./CheckoutPage.scss";
+import ScanBarcode from "../../components/ScanBarcode/ScanBarcode";
+
+function CheckoutPage() {
+  return (
+    <div>
+      <ScanBarcode />
+    </div>
+  );
+}
+import ScanBarcode from "../../components/ScanBarcode/ScanBarcode";
---
+++ src/components/ScanBarcode/ScanBarcode.jsx
@@ -0,0 +1,8 @@
+function ScanBarcode() {
+  return;
+  <div className="scanbarcode">
+    <h1>Scan Products</h1>
+  </div>;
+}
+
+export default ScanBarcode;
